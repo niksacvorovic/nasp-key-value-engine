5528990882bd3f360bf6b0c2fa9d32436da50c1b
Jovan Cvijanovic <jovan.cvijanovic2@gmail.com>
2025-06-25 23:54:38 +0200

    refactor: ⚙️ Minor Config changes

--- config/config.go
+++ config/config.go
@@ -2,16 +2,22 @@ package config
 
 // Struktura koja se podudara sa JSON strukturom
 type Config struct {
+	// Memtable
 	MaxMemtableSize  int    `json:"MaxMemtableSize"`
 	Num_memtables    int    `json:"Num_memtables"`
 	Memtable_struct  string `json:"Memtable_struct"`
 	SkipListLevelNum int    `json:"SkipListLevelNum"`
-	BlockSize        int    `json:"BlockSize"`
-	BlockCacheSize   int    `json:"BlockCacheSize"`
-	LRUCacheSize     int    `json:"LRUCacheSize"`
-	FilePath         string `json:"FilePath"`
-	TokenRate        int    `json:"TokenRate"`
-	TokenInterval    int    `json:"TokenInterval"`
+
+	// Block Manager and Block Cache
+	BlockSize      int `json:"BlockSize"`
+	BlockCacheSize int `json:"BlockCacheSize"`
+	LRUCacheSize   int `json:"LRUCacheSize"`
+
+	FilePath string `json:"FilePath"`
+
+	// Access Control
+	TokenRate     int `json:"TokenRate"`
+	TokenInterval int `json:"TokenInterval"`
 
 	// WAL
 	WalMaxRecordsPerSegment int `json:"WalMaxRecordsPerSegment"`
